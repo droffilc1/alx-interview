ba8f94860ae0fd555f65b03e855b41681573f8f4
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-17 10:31:01 +0300

    fix: log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,34 +9,36 @@ status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 try:
     for line in sys.stdin:
-        # Parse the line
-        line_parse = line.split(" ")
+        # Strip and split the line to parse the components
+        line_parse = line.strip().split(" ")
 
+        # Ensure line has enough components to avoid index errors
         if len(line_parse) > 4:
-            # Update metrics
             try:
+                # Assume the status code is the second to last element
                 status_code = int(line_parse[-2])
+                # Assume the file size is the last element
                 file_size = int(line_parse[-1])
+
+                # Update total file size
+                total_file_size += file_size
+
+                # Update status code if status code is in the dictionary
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+
+                # Update line count
+                count += 1
+
+                # Print metrics after every 10 lines
+                if count % 10 == 0:
+                    print(f"File size: {total_file_size}")
+                    for k, v in sorted(status_codes.items()):
+                        if v != 0:
+                            print(f"{k}: {v}")
+                    sys.stdout.flush()
             except (ValueError, IndexError):
                 continue  # Skip if there's an issue with parsing these values
-
-            # Check if status code exists in the given dict
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-
-            # Update total file size
-            total_file_size += file_size
-
-            # Update line count
-            count += 1
-
-            # Print metrics after every 10 lines
-            if count % 10 == 0:
-                print(f"File size: {total_file_size}")
-                for k, v in sorted(status_codes.items()):
-                    if v != 0:
-                        print(f"{k}: {v}")
-                sys.stdout.flush()
 
 except KeyboardInterrupt:
     pass

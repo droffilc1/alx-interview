a93ccfb93712c3786071ff9793ce911842f341fc
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-05 15:22:20 +0300

    chore: use request module

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,44 @@
 #!/usr/bin/node
-// prints all characters of a Star Wars movie
-const request = require('request-promise-native');
+const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 if (!movieId) {
-  console.log('Usage: ./0-starwars_character MovieID');
+  console.log('Usage: ./0-starwars_characters.js <movieId>');
   process.exit(1);
 }
 
-async function fetchCharacterName (characterUrl) {
+const makeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error('Received status code ' + response.statusCode));
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+
+const fetchCharacterName = async (url) => {
   try {
-    const characterBody = await request(characterUrl);
-    const characterData = JSON.parse(characterBody);
+    const charBody = await makeRequest(url);
+    const characterData = JSON.parse(charBody);
     return characterData.name;
   } catch (error) {
     console.error('Error fetching character:', error);
   }
-}
+};
 
-async function printCharacterNames () {
+const printCharacterNames = async () => {
   try {
-    const body = await request(url);
+    const body = await makeRequest(url);
     const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
+    const charactersUrls = filmData.characters;
 
-    for (const characterUrl of characterUrls) {
+    for (const characterUrl of charactersUrls) {
       const characterName = await fetchCharacterName(characterUrl);
       if (characterName) {
         console.log(characterName);
@@ -34,6 +47,6 @@ async function printCharacterNames () {
   } catch (error) {
     console.error('Error fetching film data:', error);
   }
-}
+};
 
 printCharacterNames();

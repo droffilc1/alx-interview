09de0d97560c4ef9639944bd895b4e4ef346e65d
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-05 14:57:08 +0300

    fix: star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,38 @@
 #!/usr/bin/node
 // prints all characters of a Star Wars movie
-const rp = require('request-promise');
+const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-async function starwarsCharacters (movieId) {
-  try {
-    const body = await rp(url);
-    const result = JSON.parse(body);
-    for (const character of result.characters) {
-      try {
-        const characterBody = await rp(character);
-        const characterResult = JSON.parse(characterBody);
-        console.log(characterResult.name);
-      } catch (characterError) {
-        console.error(`Error fetching character: ${characterError}`);
-      }
-    }
-  } catch (error) {
-    console.error(`Error fetching movie: ${error}`);
-  }
+if (!movieId) {
+  console.log('Please provide a Movie ID');
+  process.exit(1);
 }
 
-starwarsCharacters(movieId);
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error making the request:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error: Received status code', response.statusCode);
+    return;
+  }
+
+  // Parse the response body as JSON
+  const filmData = JSON.parse(body);
+  const charactersUrls = filmData.characters;
+
+  charactersUrls.forEach(url => {
+    request(url, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error('Error: Received a status code', charResponse.statusCode);
+        return;
+      }
+      // Parse the character response body as JSON
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+    });
+  });
+});
